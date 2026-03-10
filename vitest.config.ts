48b5ec4df99780f4b944df6afa7f141de312edfb
NickChecan <nicholas.checan@gmail.com>
2026-03-09 23:59:04 -0500

    chore(common): setup test coverage with vitest and v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,23 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         include: ['test/**/*.spec.ts'],
+        exclude: ['test/integration/**'],
         env: {
             SUPPRESS_NO_CONFIG_WARNING: 'true',
         },
         globals: true,
-        testTimeout: 30000
+        testTimeout: 30000,
+        coverage: {
+            provider: 'v8',
+            include: ['src/**/*.ts'],
+            exclude: ['src/index.ts'],
+            reporter: ['text', 'lcov'],
+            thresholds: {
+                branches: 80,
+                functions: 80,
+                lines: 80,
+                statements: 80,
+            },
+        },
     },
 })

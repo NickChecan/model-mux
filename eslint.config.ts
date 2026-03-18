c308c5eb2d199b3714f980479e90a1a162a8a51d
NickChecan <nicholas.checan@gmail.com>
2026-03-18 10:18:24 -0500

    fix(common): remove dist from lint scope

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { ignores: ['coverage/'] },
+  { ignores: ['coverage/', 'dist/'] },
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
